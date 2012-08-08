c0bd8e993f8aea1348b47c74f31d698368866a30
Manuel Cajada <cajadas@gmail.com>
2012-08-08 13:02:38 +0100

    Initializing ByclosureGWTPublic

---
+++ src/main/java/com/byclosure/opensource/client/util/TextTrimmer.java
@@ -0,0 +1,27 @@
+package com.byclosure.opensource.client.util;
+
+import com.google.gwt.event.logical.shared.AttachEvent;
+import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.Widget;
+
+public class TextTrimmer {
+
+	public static void trim(final HasText widget) {
+		((Widget) widget).addAttachHandler(new AttachEvent.Handler() {
+			public void onAttachOrDetach(AttachEvent event) {
+				trim0(widget);
+			}
+		});
+	}
+	
+	private static void trim0(HasText widget) {
+		int widgetHeight = ((Widget) widget).getOffsetHeight();
+		int parentHeight = ((Widget) widget).getParent().getOffsetHeight();
+
+		if (widgetHeight > parentHeight) {
+			String text = widget.getText();
+			widget.setText(text.replaceFirst("\\W*\\s(\\S)*$/?", "..."));
+			trim0(widget);
+		}
+	}
+}
